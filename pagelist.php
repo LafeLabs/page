369907118d21c9001a27ff7ceb43e4946f61a1bd
Lafe Spietz <lafelabs@gmail.com>
2019-06-15 20:13:10 -0400

    added functionality to page list to branch down to symbol/ paper/ and map/ as well as page/

--- pagelist.php
+++ pagelist.php
@@ -1,4 +1,24 @@
 <a href = "index.html">back to main</a>
+
+<table id = "newtable">
+    <tr>
+        <td>new page name:</td>
+        <td><input/></td>
+    </tr>
+    <tr>
+        <td>new map factory name:</td>
+        <td><input/></td>
+    </tr>
+    <tr>
+        <td>new symbol factory name:</td>
+        <td><input/></td>
+    </tr>
+    <tr>
+        <td>new technical paper name:</td>
+        <td><input/></td>
+    </tr>
+</table>
+
 <ul>
 <?php
 
@@ -18,8 +38,44 @@ foreach($files as $value){
 
 ?>
 </ul>
+<script>
+inputs = document.getElementById("newtable").getElementsByTagName("input");    
+
+inputs[0].onchange = function() {
+    var newa = document.createElement("A");
+    newa.innerHTML = "mkdir.php?dir=" + this.value;
+    newa.href = "mkdir.php?dir=" + this.value;
+    this.parentNode.appendChild(newa);
+}
+    
+inputs[1].onchange = function() {
+    var newa = document.createElement("A");
+    newa.innerHTML = "mkmap.php?dir=" + this.value;
+    newa.href = "mkmap.php?dir=" + this.value;
+    this.parentNode.appendChild(newa);
+}
+
+inputs[2].onchange = function() {
+    var newa = document.createElement("A");
+    newa.innerHTML = "mksymbol.php?dir=" + this.value;
+    newa.href = "mksymbol.php?dir=" + this.value;
+    this.parentNode.appendChild(newa);
+}
+
+
+inputs[3].onchange = function() {
+    var newa = document.createElement("A");
+    newa.innerHTML = "mkpaper.php?dir=" + this.value;
+    newa.href = "mkpaper.php?dir=" + this.value;
+    this.parentNode.appendChild(newa);
+}
+    
+</script>
 <style>
     body{
         font-size:2em;
+    }
+    table{
+        font-family:courier;
     }
 </style>
